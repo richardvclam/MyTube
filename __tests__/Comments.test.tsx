bc5b2df0e435b1fc9c7856821e6b06cd53b36387
Richard Lam <richardvclam@gmail.com>
2019-08-10 00:28:29 -0700

    Fixing up unit test and comments

--- __tests__/Comments.test.tsx
+++ __tests__/Comments.test.tsx
@@ -12,23 +12,36 @@ import { Text } from 'react-native';
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-// it('renders correctly', () => {
-//   mockedAxios.get.mockResolvedValue({ data: resolvedComments });
-//   const tree = renderer.create(<Comments videoId="WZSuU1gdJTg" />).toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
-
-it('renders correctly with a valid video id', done => {
+it('renders correctly when API call resolves', done => {
+  // Set the mock resolved value
+  // This is the equivalent to .then in axios.get().then()
   mockedAxios.get.mockResolvedValue({ data: resolvedComments });
-  const CommentsComponent = renderer.create(<Comments videoId="WZSuU1gdJTg" />);
+
+  // Render the Comments component and wait for 500
+  const CommentsComponent = renderer.create(
+    <Comments videoId="somethingright" />
+  );
+
   setTimeout(() => {
+    // Update the snapshot
     const tree = CommentsComponent.toJSON();
     expect(tree).toMatchSnapshot();
+
+    // Get the instance so we can access the state
+    const instance = CommentsComponent.getInstance() as renderer.ReactTestInstance &
+      Comments;
+    if (instance) {
+      expect(instance.state.comments.length).toBe(2);
+      expect(instance.state.error).toBe(false);
+      expect(instance.state.fetching).toBe(false);
+    }
+
+    // Manually callback so we can determine when this test is actually done
     done();
   }, 500);
 });
 
-it('renders throws error with invalid video id', done => {
+it('renders error when API call rejects', done => {
   mockedAxios.get.mockRejectedValue({ data: rejectedComments });
   const CommentsComponent = renderer.create(
     <Comments videoId="somethingwrong" />
